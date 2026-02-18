5393073e32018580953d43e57a1952912cd6e98f
abzz700 <abdulazizabutalipov@gmail.com>
2026-02-18 13:23:47 +0500

    Abu
    #Abutalipov

---
+++ proekt640FIO.py
@@ -0,0 +1,14 @@
+def funk320FIO(x, y):
+    
+
+    if x % 2 == 0:
+        result_x = "Четное"   
+    else:
+        result_x = "Нечетное"    
+
+    if y % 2 == 0:
+        result_y = "Четное"
+    else:
+        result_y = "Нечетное"
+
+    return result_x, result_y
